6a1eaffcf521ff4f006127bdb2f991a38701d618
Aergiaaa <muzakren45@gmail.com>
2026-02-07 01:34:51 +0700

    feat: finally adding runtime execution to a .il file extension

--- main.go
+++ main.go
@@ -4,11 +4,49 @@ import (
 	"fmt"
 	"os"
 	"os/user"
+	"path/filepath"
 
+	"github.com/Aergiaaa/idiotic_interpreter/evaluator"
+	"github.com/Aergiaaa/idiotic_interpreter/lexer"
+	"github.com/Aergiaaa/idiotic_interpreter/object"
+	"github.com/Aergiaaa/idiotic_interpreter/parser"
 	"github.com/Aergiaaa/idiotic_interpreter/repl"
 )
 
 func main() {
+	if len(os.Args) >= 3 && os.Args[1] == "run" {
+		filename := os.Args[2]
+
+		// check the extension
+		if filepath.Ext(filename) != ".il" {
+			fmt.Fprintf(os.Stderr, "Error file must have .il extension\n")
+			os.Exit(1)
+		}
+
+		// exec file mode
+		content, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+			os.Exit(1)
+		}
+		input := string(content)
+
+		env := object.InitEnv()
+		l := lexer.InitLexer(input)
+		p := parser.InitParser(l)
+		program := p.Parse()
+
+		if len(p.Errors()) != 0 {
+			for _, err := range p.Errors() {
+				fmt.Fprintf(os.Stderr, "Parser Error: %s\n", err)
+			}
+			os.Exit(1)
+		}
+
+		evaluator.Eval(program, env)
+		return
+	}
+
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
